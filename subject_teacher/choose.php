48f1a9eb46194f0732def811ed01da3e69bd1e15
Guilherme Samuel <guisamuel800@gmail.com>
2022-08-13 22:43:46 -0300

    updating insert without FK at form

--- subject_teacher/choose.php
+++ subject_teacher/choose.php
@@ -5,20 +5,18 @@
 <html lang="pt">
     <head>
 		<?php include ('../head.html'); ?>
-        <title>Selecionar Sala</title>
+        <title>Selecionar Matéria</title>
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 		<script src="//cdnjs.cloudflare.com/ajax/libs/jquery.maskedinput/1.4.1/jquery.maskedinput.min.js"></script>
     </head>
     <body>
-        <h1 style="margin: 40px;">Selecione uma sala</h1>
+        <h1 style="margin: 40px;">Selecione uma matéria</h1>
         <?php
-            $sql = "SELECT classroom.id, grade.name, period.name from classroom
-            INNER JOIN period ON period.id = classroom.period_id
-            INNER JOIN grade ON grade.id = classroom.grade_id;";
+            $sql = "SELECT id, name FROM subject;";
             $query = mysqli_query($connection, $sql);
             while($row = mysqli_fetch_row($query)) {
-                echo "<a href='student.php?id=".$row[0]."'>";
-                echo "<input style='margin: 20px;' type='submit' value='".$row[1]." - ".$row[2]."'>";
+                echo "<a href='subject_teacher.php?id=".$row[0]."'>";
+                echo "<input style='margin: 20px;' type='submit' value='".$row[1]."'>";
                 echo "</a>";
             }
         ?>
--- subject_teacher/subject_teacher.php
+++ subject_teacher/subject_teacher.php
@@ -1,9 +1,9 @@
 <?php
     include '../connection.php';
+	$subject_id = $_GET['id'];
     if(isset($_POST['submit'])) {
 		$teacher = $_POST['teacher'];
-		$subject = $_POST['subject'];
-        $sql = "INSERT INTO subject_teacher (teacher_id, subject_id) VALUES ($teacher, $subject)";
+        $sql = "INSERT INTO subject_teacher (teacher_id, subject_id) VALUES ($teacher, $subject_id)";
         $query = mysqli_query($connection, $sql);
     }
     else if(isset($_POST['delete'])) {
@@ -34,22 +34,11 @@
 						}
 					?>
 				</select><br>
-				<label for="subject">Matéria/Disciplina:</label>
-				<select name="subject" id="subject">
-					<?php
-						$sql = "SELECT * FROM subject ORDER BY id";
-						$query = mysqli_query($connection, $sql);
-						while($row = mysqli_fetch_assoc($query)) {
-							$id = $row['id'];
-							$name = $row['name'];
-							echo '<option value="'.$id.'">'.$name.'</option>';
-						}
-					?>
-				</select><br>
-                <input type="submit" name="submit" value="Enviar">
+                <input type="submit" name="submit" value="Enviar"><br>
+				<input type="text" placeholder="Pesquisar por nome" id="searchbar" onkeyup="filter();">
             </div>
             <div class="list">
-                <table>
+                <table id="list_table">
                     <tr>
 						<th>Professor</th>
 						<th>Matéria</th>
@@ -63,15 +52,16 @@
 							if($row[0] != 0) {
 								$sql = "SELECT subject_teacher.id, teacher.id, teacher.name, subject.name FROM subject_teacher
 								INNER JOIN teacher ON teacher.id = subject_teacher.teacher_id
-								INNER JOIN subject ON subject.id = subject_teacher.subject_id";
+								INNER JOIN subject ON subject.id = subject_teacher.subject_id
+								WHERE subject_teacher.subject_id = $subject_id";
 								$query = mysqli_query($connection, $sql);
 								while($column = mysqli_fetch_row($query)) {
 									$id = $column[0];
 									$teacher_id = $column[1];
 									$teacher_name = $column[2];
 									$subject_name = $column[3];
-									echo '<tr>';
-									echo '<td>'.$teacher_id.' - '.$teacher_name.'</td>';
+									echo '<tr class="tb_search">';
+									echo '<td class="tb_name">'.$teacher_id.' - '.$teacher_name.'</td>';
 									echo '<td>'.$subject_name.'</td>';
 									echo '<td><button name="delete" value="'.$id.'">Deletar</button>';
 									echo '<a href="edit.php?id='.$id.'"><input type="button" value="Editar"></a></td>';
