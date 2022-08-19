37fb929eddddcd4567c3d6a77585d476a37ef117
Guilherme Samuel <guisamuel800@gmail.com>
2022-08-19 18:19:03 -0300

    finishing query execution

--- student_absence/add_day.php
+++ student_absence/add_day.php
@@ -5,19 +5,19 @@
 
     $day = $_POST['day'];
     $subject_teacher = $_POST['subject_teacher'];
-
-    echo $day;
-    echo $subject_teacher;
     
     if(isset($_POST['submit'])) {
         $i = 0;
         $state = $_POST['state'];
+        $day = $_POST['day'];
+        $subject_teacher = $_POST['subject_teacher'];
         $sql = "SELECT id FROM student WHERE classroom_id = '$classroom_id';";
         $query = mysqli_query($connection, $sql);
         while($row = mysqli_fetch_row($query)) {
             $states = explode(":", $state[$i]);
-            $sql = "INSERT INTO student_absence (student_id, day, state, subject_teacher_id)
+            $sql_a = "INSERT INTO student_absence (student_id, day, state, subject_teacher_id)
             VALUES ('$states[1]', '$day', '$states[0]', '$subject_teacher');";
+            mysqli_query($connection, $sql_a);
             $i++;
         }
     }
@@ -30,10 +30,10 @@
 		<script src="//cdnjs.cloudflare.com/ajax/libs/jquery.maskedinput/1.4.1/jquery.maskedinput.min.js"></script>
     </head>
     <body>
-        <a href="../index.php"><button>Voltar</button></a>
+        <button onclick="history.go(-1);">Voltar</button>
         <h1 style="margin: 40px;">Controlar Presença e Ausência</h1>
         <form method="POST">
-            <input type="submit" value="Enviar Chamada"/>
+            <input type="submit" name="submit" value="Enviar Chamada">
             <table id="list_table">
                 <tr>
                     <th>Aluno</th>
@@ -53,6 +53,8 @@
                     mysqli_close($connection);
                 ?>
             </table>
+            <input type="date" style="visibility: hidden;" value="<?php echo $day; ?>" name="day">
+            <input type="number" style="visibility: hidden;" value="<?php echo $subject_teacher; ?>" name="subject_teacher">
         </form>
     </body>
 </html>
