64978463931d5bd2d33f4ed91e4d4f0ff62f0f1f
Guilherme Samuel <guisamuel800@gmail.com>
2022-08-20 22:10:21 -0300

    organizing & aligning tables

--- student_absence/student_absence.php
+++ student_absence/student_absence.php
@@ -8,26 +8,31 @@
         <title>Controle de Faltas e Presenças</title>
     </head>
     <body>
-		<a href="../index.php"><button>Voltar</button></a>
-		<input type="text" style="margin-top: 20px;" placeholder="Pesquisar por nome" id="searchbar" onkeyup="filter();">
+		<button onclick="history.go(-1);">Voltar</button>
         <form method="POST">
             <div class="list">
-				<table id="list_table">		
+				<table>		
 					<tr>
 						<th>Professor</th>
 						<?php
-							$sql = "SELECT day FROM student_absence 
+							$lines = array();
+							$sql = "SELECT day FROM student_absence
 							WHERE student_id = '$student_id' ORDER BY subject_teacher_id, id;";
 							$query = mysqli_query($connection, $sql);
 							while($row = mysqli_fetch_row($query)) {
-								echo '<th>'.$row[0].'</th>';
+								$d = '<th>'.$row[0].'</th>';
+								array_push($lines, $d);
+								echo $d;
 							}
 						?>
 					</tr>
 					<?php
+						$not_found = "Sem aula";
 						$teacher = "";
 						$can = False;
-						$sql = "SELECT student_absence.subject_teacher_id, student_absence.state, teacher.name, subject.name FROM student_absence
+						$i = 0;
+						$sql = "SELECT student_absence.subject_teacher_id, student_absence.state, 
+						teacher.name, subject.name, student_absence.day FROM student_absence
 						INNER JOIN subject_teacher ON student_absence.subject_teacher_id = subject_teacher.id
 						INNER JOIN teacher ON teacher.id = subject_teacher.teacher_id
 						INNER JOIN subject ON subject.id = subject_teacher.subject_id
@@ -36,15 +41,33 @@
 						$query = mysqli_query($connection, $sql);
 						while($row = mysqli_fetch_row($query)) {
 							if($teacher != $row[2]) {
-								if($can) echo '</tr>';
+								if($can) {
+									if($i < count($lines)) {
+										for($j = $i; $j < count($lines); $j++)
+											echo '<td>'.$not_found.'</td>';
+									}
+									echo '</tr>';
+								}
 								echo '<tr class="tb_search">';
 								echo '<td class="tb_name">'.$row[2].' - '.$row[3].'</td>';
 								$teacher = $row[2];
+								$i = 0;
 							}
-							if($row[1] == "P")
-								echo '<td>Presente</td>';
-							else
-								echo '<td>Ausente</td>';
+							preg_match('/<th>(.*?)<\/th>/s', $lines[$i], $match);
+							$i++;
+							while($match[1] != strval($row[4])) {
+								echo '<td>'.$not_found.'</td>';
+								preg_match('/<th>(.*?)<\/th>/s', $lines[$i], $match);
+								$i++;
+							}
+							
+							if($match[1] == strval($row[4])) {
+								if($row[1] == "P")
+									echo '<td style="background-color: #5fda57;" class="present">Presente</td>';
+								else if($row[1] == "A")
+									echo '<td style="background-color: #da5757;" class="absent">Ausente</td>';
+							}
+
 							$can = True;
 						}
 					?>
