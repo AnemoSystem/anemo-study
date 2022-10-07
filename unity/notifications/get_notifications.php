b188002889725914b98ca43d6aebf8811f097679
Guilherme Samuel <guisamuel800@gmail.com>
2022-10-06 23:50:06 -0300

    update query

--- unity/notifications/get_notifications.php
+++ unity/notifications/get_notifications.php
@@ -2,16 +2,22 @@
     include '../../connection.php';
     if(isset($_POST['username'])) {
         $username = $_POST['username'];
+        
+        $sql = "SELECT id FROM user WHERE user_nickname = '$username';";
+        $query = mysqli_query($connection, $sql);
+        $row = mysqli_fetch_row($query);
+        $id = $row[0];
+
         $sql = "SELECT notifications.id,
         notifications.title, notifications.type, 
-        notifications.from_user, notifications.send_date
-        FROM notifications 
-        INNER JOIN user ON user.id = notifications.to_user
-        WHERE user.user_nickname = '$username'";
+        notifications.send_date, user.user_nickname
+        FROM notifications
+        INNER JOIN user ON user.id = notifications.from_user
+        WHERE notifications.to_user = '$id';";
         $query = mysqli_query($connection, $sql);
 
         while($row = mysqli_fetch_assoc($query)) {
-            echo $row['id']."@".$row['title']."@".$row['type']."@".$row['from_user']."@".$row['send_date'];
+            echo $row['id']."@".$row['title']."@".$row['type']."@".$row['user_nickname']."@".$row['send_date'];
             echo "%";
         }
     }
