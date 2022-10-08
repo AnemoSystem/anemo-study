212e084eeff93b431797a497e6db01c86a5d73ed
Guilherme Samuel <guisamuel800@gmail.com>
2022-10-08 18:23:35 -0300

    adapt for 'from_id' and limit query results

--- unity/notifications/get_notifications.php
+++ unity/notifications/get_notifications.php
@@ -2,19 +2,36 @@
     include '../../connection.php';
     if(isset($_POST['username'])) {
         $username = $_POST['username'];
-        
+        $from_id = $_POST['from_id'];
+
         $sql = "SELECT id FROM user WHERE user_nickname = '$username';";
         $query = mysqli_query($connection, $sql);
         $row = mysqli_fetch_row($query);
         $id = $row[0];
-
-        $sql = "SELECT notifications.id,
-        notifications.title, notifications.type, 
-        notifications.send_date, user.user_nickname,
-        notifications.status
-        FROM notifications
-        INNER JOIN user ON user.id = notifications.from_user
-        WHERE notifications.to_user = '$id';";
+        
+        $from_id = intval($from_id);
+        if($from_id == 0) {
+            $sql = "SELECT notifications.id,
+            notifications.title, notifications.type, 
+            notifications.send_date, user.user_nickname,
+            notifications.status
+            FROM notifications
+            INNER JOIN user ON user.id = notifications.from_user
+            WHERE notifications.to_user = '$id'
+            ORDER BY notifications.id DESC
+            LIMIT 4;";
+        } else {
+            $sql = "SELECT notifications.id,
+            notifications.title, notifications.type, 
+            notifications.send_date, user.user_nickname,
+            notifications.status
+            FROM notifications
+            INNER JOIN user ON user.id = notifications.from_user
+            WHERE notifications.to_user = '$id' AND
+            notifications.id < '$from_id'
+            ORDER BY notifications.id DESC
+            LIMIT 4;";            
+        }
         $query = mysqli_query($connection, $sql);
 
         while($row = mysqli_fetch_assoc($query)) {
--- unity/notifications/get_number_notifications.php
+++ unity/notifications/get_number_notifications.php
@@ -2,9 +2,20 @@
     include '../../connection.php';
     if(isset($_POST['username'])) {
         $username = $_POST['username'];
-        $sql = "SELECT COUNT(*) FROM notifications 
-        INNER JOIN user ON user.id = notifications.to_user
-        WHERE user.user_nickname = '$username'";
+        $from_id = $_POST['from_id'];
+
+        $from_id = intval($from_id);
+
+        if($from_id != 0) {
+            $sql = "SELECT COUNT(*) FROM notifications 
+            INNER JOIN user ON user.id = notifications.to_user
+            WHERE user.user_nickname = '$username' AND
+            notifications.id < '$from_id'";
+        } else {
+            $sql = "SELECT COUNT(*) FROM notifications 
+            INNER JOIN user ON user.id = notifications.to_user
+            WHERE user.user_nickname = '$username'";            
+        }
         $query = mysqli_query($connection, $sql);
         $result = mysqli_fetch_row($query);
         
