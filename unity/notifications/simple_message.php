1a233931ba3f88cae2e5017ff66faae1e5050250
Guilherme Samuel <guisamuel800@gmail.com>
2022-10-12 19:37:12 -0300

    view message (read & delete)

---
+++ unity/notifications/simple_message.php
@@ -0,0 +1,18 @@
+<?php
+    include '../../connection.php';
+    if(isset($_POST['id'])) {
+        $id = $_POST['id'];
+        $sql = "SELECT notifications.title, notifications.message, user.user_nickname,
+        notifications.send_date FROM notifications
+        INNER JOIN user ON user.id = notifications.from_user
+        WHERE notifications.id = '$id';";
+        $query = mysqli_query($connection, $sql);
+        $result = mysqli_fetch_assoc($query);
+
+        echo $result['title']."=".$result['message']."=".$result['user_nickname']."=".$result['send_date'];
+    
+        $sql = "UPDATE notifications SET status = 'R' WHERE id = '$id';";
+        $query = mysqli_query($connection, $sql);    
+    }
+    $connection->close();
+?>
