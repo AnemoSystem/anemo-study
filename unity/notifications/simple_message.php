8f72fb2e044c6f847337292605d7636f69a0784e
Guilherme Samuel <guisamuel800@gmail.com>
2022-10-13 11:23:34 -0300

    add type at sql query

--- unity/notifications/simple_message.php
+++ unity/notifications/simple_message.php
@@ -3,13 +3,13 @@
     if(isset($_POST['id'])) {
         $id = $_POST['id'];
         $sql = "SELECT notifications.title, notifications.message, user.user_nickname,
-        notifications.send_date FROM notifications
+        notifications.send_date, notifications.type FROM notifications
         INNER JOIN user ON user.id = notifications.from_user
         WHERE notifications.id = '$id';";
         $query = mysqli_query($connection, $sql);
         $result = mysqli_fetch_assoc($query);
 
-        echo $result['title']."=".$result['message']."=".$result['user_nickname']."=".$result['send_date'];
+        echo $result['title']."=".$result['message']."=".$result['user_nickname']."=".$result['send_date']."=".$result['type'];
     
         $sql = "UPDATE notifications SET status = 'R' WHERE id = '$id';";
         $query = mysqli_query($connection, $sql);    
