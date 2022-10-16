bafe14d7ffe98e260295ed1f1dea67db7696be1c
Guilherme Samuel <guisamuel800@gmail.com>
2022-10-15 23:59:46 -0300

    order by

--- unity/list_friends.php
+++ unity/list_friends.php
@@ -11,14 +11,16 @@
         if($order == '1') {
             $sql = "SELECT user.id, user.user_nickname, user.is_logged FROM user
             INNER JOIN friends ON user.id = friends.user_1
-            WHERE friends.user_2 = '$id_user[0]';";
+            WHERE friends.user_2 = '$id_user[0]'
+            ORDER BY user.id;";
             $query = mysqli_query($connection, $sql);
             while($result = mysqli_fetch_row($query))
                 echo $result[0]."=".$result[1]."=".$result[2]."*";
         } else {
             $sql = "SELECT user.id, user.user_nickname, user.is_logged FROM user
             INNER JOIN friends ON user.id = friends.user_2
-            WHERE friends.user_1 = '$id_user[0]';";
+            WHERE friends.user_1 = '$id_user[0]'
+            ORDER BY user.id;";
             $query = mysqli_query($connection, $sql);
             while($result = mysqli_fetch_row($query))
                 echo $result[0]."=".$result[1]."=".$result[2]."*";
