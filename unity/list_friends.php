e356d1948b5f485ef086d4f98d650815df60667c
Guilherme Samuel <guisamuel800@gmail.com>
2022-10-16 16:07:39 -0300

    refactor id variable and display 'error'

--- unity/list_friends.php
+++ unity/list_friends.php
@@ -7,23 +7,31 @@
         $sql_user = "SELECT id FROM user WHERE user_nickname = '$username';";
         $query_user = mysqli_query($connection, $sql_user);
         $id_user = mysqli_fetch_row($query_user);
+        
+        $id = $id_user[0];
 
         if($order == '1') {
             $sql = "SELECT user.id, user.user_nickname, user.is_logged FROM user
             INNER JOIN friends ON user.id = friends.user_1
-            WHERE friends.user_2 = '$id_user[0]'
+            WHERE friends.user_2 = '$id'
             ORDER BY user.id;";
             $query = mysqli_query($connection, $sql);
-            while($result = mysqli_fetch_row($query))
-                echo $result[0]."=".$result[1]."=".$result[2]."*";
+            if(mysqli_num_rows($query) > 0) {
+                while($result = mysqli_fetch_row($query)) {
+                    echo $result[0]."=".$result[1]."=".$result[2]."*";
+                }
+            } else echo "error";
         } else {
             $sql = "SELECT user.id, user.user_nickname, user.is_logged FROM user
             INNER JOIN friends ON user.id = friends.user_2
-            WHERE friends.user_1 = '$id_user[0]'
+            WHERE friends.user_1 = '$id'
             ORDER BY user.id;";
             $query = mysqli_query($connection, $sql);
-            while($result = mysqli_fetch_row($query))
-                echo $result[0]."=".$result[1]."=".$result[2]."*";
+            if(mysqli_num_rows($query) > 0) {
+                while($result = mysqli_fetch_row($query)) {
+                    echo $result[0]."=".$result[1]."=".$result[2]."*";
+                }
+            } else echo "error";
         }
     }
     $connection->close();
