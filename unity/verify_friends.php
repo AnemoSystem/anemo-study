3d9a7cfab9da2a0cf75b1fa7eacb59a86b72761f
Guilherme Samuel <guisamuel800@gmail.com>
2022-10-26 11:32:27 -0300

    verify friends by id (sql)

--- unity/verify_friends.php
+++ unity/verify_friends.php
@@ -4,15 +4,23 @@
     $user_1 = $_POST['user_1'];
     $user_2 = $_POST['user_2'];
 
+    $sql1 = "SELECT id FROM user WHERE user_nickname = '$user_1'";
+    $query1 = mysqli_query($connection, $sql1);
+    $r1 = mysqli_fetch_row($query1);
+
+    $sql2 = "SELECT id FROM user WHERE user_nickname = '$user_2'";
+    $query2 = mysqli_query($connection, $sql2);
+    $r2 = mysqli_fetch_row($query2);
+
     $sql = "SELECT id FROM friends 
-    WHERE user_1 = '$user_1' AND user_2 = '$user_2'";
+    WHERE user_1 = '$r1[0]' AND user_2 = '$r2[0]'";
     $query = mysqli_query($connection, $sql);
 
     if(mysqli_num_rows($query) > 0)
         echo "exists";
     else {
         $sql = "SELECT id FROM friends 
-        WHERE user_2 = '$user_1' AND user_1 = '$user_2'";
+        WHERE user_2 = '$r1[0]' AND user_1 = '$r2[0]'";
         $query = mysqli_query($connection, $sql);
         if(mysqli_num_rows($query) > 0)
             echo "exists";
